8561c52f41ad45e576533eb66411b9982d46e8fb
Purnachandra Mallick <purnachandram674@gmail.com>
2025-10-08 10:23:20 +0530

    Time: 1 ms (78.02%), Space: 45.7 MB (86.32%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        // Loop through prices from day 1
+        for (int i = 1; i < prices.length; i++) {
+            // If today's price is higher than yesterday, sell for profit
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
