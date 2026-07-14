9d0b3c7a8c7819791d991e4ec30e23ac2d7dcc1a
Purnachandra Mallick <purnachandram674@gmail.com>
2026-07-14 13:08:43 +0530

    Time: 21 ms (10.19%), Space: 48.2 MB (99.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,22 @@
-import java.util.*;
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) return new ArrayList<>();
-        
-        Map<String, List<String>> map = new HashMap<>();
-        
-        for (String s : strs) {
-            char[] chars = s.toCharArray();
-            Arrays.sort(chars);  // sort characters in the string
-            String key = new String(chars);  // use sorted string as key
-            
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
+        Map<String, List> map = new HashMap<>();
+        for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[ c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i : count){
+                sb.append("#");
+                sb.append(i);
+            }
+            String key = sb.toString();
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<String>());
             }
             map.get(key).add(s);
         }
-        
-        return new ArrayList<>(map.values());
+        return new ArrayList(map.values());
     }
 }
